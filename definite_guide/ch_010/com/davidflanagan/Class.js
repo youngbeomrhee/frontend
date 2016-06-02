29e7931a5b35f79ac01f7da8c6f8edf24547b834
YB <seektruthyb@naver.com>
2016-06-03 00:34:06 +0900

    샘플정리

---
+++ definite_guide/ch_010/com/davidflanagan/Class.js
@@ -0,0 +1,17 @@
+/**
+ * Created by YB on 2016-06-02.
+ */
+
+if(com.davidflanagan.Class) {
+    throw new Error("com.davidflanagan.Class already exists");
+} else {
+    com.davidflanagan.Class = {
+        define: function (data) {
+            /* 코드*/
+        }
+        , provides: function (o, c) {
+            /* 코드 */
+        }
+    }
+}
+
