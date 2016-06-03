189126b7ceecd14e0537972c349f5a1d817bc9ab
yblee <youngbeomrhee@gmail.com>
2016-06-03 15:50:32 +0900

    모듈완성

---
+++ definite_guide/ch_010_refact/js/com/yb/counterVo.js
@@ -0,0 +1,30 @@
+/**
+ * Created by yblee on 2016-06-03.
+ */
+
+if(!com.yb.counterVo) {
+    com.yb.counterVo = {};
+} else {
+    createObjException('com.yb.counterVo');
+}
+
+(function(){
+    var cntVo = com.yb.counterVo;
+
+    var counter = 0;
+
+    cntVo.getCounter = function() {
+        console.log('counter : ' + counter);
+        return counter;
+    }
+
+    cntVo.addCounter = function() {
+        console.log('counter++');
+        counter++;
+    }
+
+    cntVo.resetCounter = function() {
+        console.log('counter\'s initialized');
+        counter = 0;
+    }
+})();
