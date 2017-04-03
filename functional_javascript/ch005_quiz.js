495286e9c42e6b307753df77d22ff3659063a584
yblee <youngbeomrhee@gmail.com>
2017-04-03 18:24:44 +0900

    예제 추가

---
+++ functional_javascript/ch005_quiz.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yblee on 2017-04-03.
+ */
+
+/*
+  1. 자료형에 따른 다형성을 구현할 수 있는 polyToStr() 함수 만들기
+  
+  polyToString(1) : 1
+  polyToString('a') : a
+  polyToString(_.range(10)) : [0,1,2,3,4,5,6,7,8,9]
+  polyToString({a:1, b:2, c:'str'}) : {"a":1,"b":2,"c":"str"}
+  polyToString(false) : false
+  
+  
+  
+*/
+
+
+
