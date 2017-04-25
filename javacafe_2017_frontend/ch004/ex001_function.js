c6bc4217583c654d5300d118985a9bed44baab38
whybe <youngbeomrhee@gmail.com>
2017-04-26 00:39:28 +0900

    예제 추가

--- javacafe_2017_frontend/ch004/ex001_function.js
+++ javacafe_2017_frontend/ch004/ex001_function.js
@@ -86,8 +86,10 @@ var addAll = handleAll(function () { return Array.prototype.reduce.call(argument
     console.log(global === window);
 })(this);
 
+
 // 일종의 sandbox 패턴이 된다
 (function (jq, us) {
+    // 이렇게 사용하면 전역 scope와 다른 새로운 scope이 생긴다
     console.log(Boolean(jq.ajax));
     console.log(Boolean(us.map));
 })($, _);
@@ -95,9 +97,63 @@ var addAll = handleAll(function () { return Array.prototype.reduce.call(argument
 
 // p.86. 즉시 실행 함수의 반환 값
 var initVal = (function () {
-    var result = '처음에 한 번만 실행해서 받아오면 되는 값';
+    var result = '이런 저런 연산/통신 끝에 받아오는 값';
     return result;
 })();
 console.log(initVal);
 
+
+// p.88. 즉시 객체 초기화
+({
+    // 여기에 설정 값(설정 상수)들을 정의할 수 있다
+    maxwidth: 600,
+    maxheight: 400,
+
+    // 유틸리티 메서드 또한 정의할 수 있다.
+    gimmeMax: function () {
+        return this.maxwidth + ' x ' + this.maxheight;
+    },
+
+    // 초기화
+    init: function () {
+        console.log(this.gimmeMax());
+        // 더 많은 초기화 작업들..
+    }
+}).init();
+
+
+/* p.92. 함수 프로퍼티 - 메모이제이션(Memoization) 패턴 */
+var myFunc = function (param) {
+    var result;
+    if (!myFunc.cache[param]) {
+        console.log(param + '(으)로 비용이 많이 드는 연산 수행');
+        result = param + ' 연산결과';
+        myFunc.cache[param] = result;
+    } else {
+        console.log(param + '(으)로 cache 된 결과 사용');
+        result = myFunc.cache[param]
+    }
+    return result;
+};
+
+// 캐시 저장공간
+myFunc.cache = {};
+
+// TODO : 일반적으로 cache를 외부로 노출시키는건 좋은 패턴이 아니다. 외부에서 접근할 수 없는 형식으로 바꿔보자
+
+var myFunc = (function () {
+    var cache = {};
+    return function(param) {
+        var result;
+        if (!cache[param]) {
+            console.log(param + '(으)로 비용이 많이 드는 연산 수행');
+            result = param + ' 연산결과';
+            cache[param] = result;
+        } else {
+            console.log(param + '(으)로 cache 된 결과 사용');
+            result = cache[param];
+        }
+        return result;
+    }
+})();
 
