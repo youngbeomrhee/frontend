117949680f1c86a094fbbdbfbbb31aa319567185
whybe <youngbeomrhee@gmail.com>
2017-05-26 19:30:54 +0900

    예제 추가

--- javacafe_2017_frontend/ch007/ex001_Array.js
+++ javacafe_2017_frontend/ch007/ex001_Array.js
@@ -78,21 +78,24 @@ range(10, 1).map(ele => ele * ele);
 // Q. applicants 중에서 이름만 추출
 
 /*-------------------------------------------------------------- reduce */
-// 인자를 줄여가며 하나의 값으로 수렴
+// 인자를 줄여가며 하나의 값으로 수렴// 기본
 range(10).reduce((accumulator, currentValue) => accumulator + currentValue);
-range(10).reduce((accumulator, currentValue, index, array) => accumulator + currentValue, 0);
-range(10).reduce((accumulator, currentValue, index, array) => accumulator + currentValue, 0);
+// 초기값 지정
+range(10).reduce((accumulator, currentValue) => accumulator + currentValue, 10);
+// index 활용
+range(10).reduce((accumulator, currentValue, index) => { console.log('now index : ' + index); return accumulator + currentValue;}, 0);
+// 원본 배열 활용
+range(10).reduce((accumulator, currentValue, index, arr) => { accumulator.push(arr.slice(index)); return accumulator; }, []);
 
 // Q. 1~10을 인자로 가진 배열의 모든 숫자를 곱하기.
 // range(10, 1).reduce( ... );
 
-// Q. [1,2,3,4,5]을 인덱스를 키로 갖고 현재 값의 제곱을 값으로 갖는 배열로 변형
+// Q. [1,2,3,4,5] 배열을 인덱스를 키로 갖고 현재 값의 제곱을 값으로 갖는 배열로 변형
 
 // Q. 추상화하여 로직을 주입하는 방식으로 바꾸기
 // function reduceFunc(arr, func) { ... }
 // 이런 식으로 실행 reduceFunc([1,2,3], (accum, curr)=>accum+curr);
 
-// Q. (accum, curr)=>accum+curr 로직을 함수로 빼보자
 // Q. 로직은 고정시키고 배열만 동적으로 받을 수 있도록 바꿔보자
 // function setReduce( ... ) { ... };
 // var reducePlusExec = setReduce( ... );
@@ -140,21 +143,29 @@ function compare(a, b) {
 // [12,23,5,2,,6,3,4,2]을 숫자의 대소비교로 정렬
 [12,23,5,2,6,3,4,2].sort((a, b) => a-b);
 
-
 /*-------------------------------------------------------------- slice */
 // 배열을 시작인덱스부터 종료 인덱스까지 자르는 새로운 배열 리턴
 // [].slice(시작인덱스, 종료인덱스)
 // 원본을 건드리지 않는다
 var arr = [1, 2, 3, 4, 5];
-arr.slice(1, 3);
+var newArr = arr.slice(1, 3);
+console.log(newArr);
 console.log(arr);
 
 
 /*-------------------------------------------------------------- splice */
 // 배열을 시작인덱스부터 갯수만큼 삭제(삭제된 값 리턴)
 // [].slice(시작인덱스, 삭제할 갯수, 추가값1, 추가값2, ...)
-arr.splice(1, 3, 7, 8);
+var newArr = arr.splice(1, 3);
+console.log(newArr);
 console.log(arr);
+
+// 값을 추가하는 경우
+arr = [1, 2, 3, 4, 5];
+newArr = arr.splice(1, 3, 7, 8);
+console.log(newArr);
+console.log(arr);
+
 
 /* 종합문제 */
 var data = range(100, 1);
@@ -169,16 +180,19 @@ var data = range(100, 1);
 
 // Q. 해당 데이터가 모두 5의 배수인지 확인
 
-// Q. 해당 데이터중 15가 하나라도 있는지 확인
+// Q. 해당 데이터중 하나라도 3의 배수인지 확인
 
-// Q. 해당 데이터의 15의 인덱스 찾기
+// Q. 해당 데이터의 3의 배수인 값 가져오기
 
-// Q. 해당 데이터를 인덱스를 키로, 요소를 배열로 변경
+// Q. 해당 데이터의 3의 배수인 값의 인덱스 가져오기
+
+// Q. 해당 데이터를 아래와 같은 형태로 바꾸기
+// [{idx:0, value:100}, {idx:1, value:50} ... ];
+
+// Q. 해당 데이터를 아래와 같은 형태로 바꾸기
 // 예 : [ {0: 100}, {1: 50}, ...]
 
-// Q. 해당 데이터의 모든 값을 더하기
 
-// Q. 첫 번째 요소 newData에서 받아오고 원본은 지우기
 
 
 
--- javacafe_2017_frontend/ch007/ex001_Array_with_answer.js
+++ javacafe_2017_frontend/ch007/ex001_Array_with_answer.js
@@ -94,11 +94,20 @@ applicants = applicants.map(ele => {
 
 /*-------------------------------------------------------------- reduce */
 // 인자를 줄여가며 하나의 값으로 수렴
+// 기본
 range(10).reduce((accumulator, currentValue) => accumulator + currentValue);
-range(10).reduce((accumulator, currentValue, index, array) => accumulator + currentValue, 0);
-range(10).reduce((accumulator, currentValue, index, array) => accumulator + currentValue, 0);
+// 초기값 지정
+range(10).reduce((accumulator, currentValue) => accumulator + currentValue, 10);
+// index 활용
+range(10).reduce((accumulator, currentValue, index) => { console.log('now index : ' + index); return accumulator + currentValue;}, 0);
+// 원본 배열 활용
+range(10).reduce((accumulator, currentValue, index, arr) => { accumulator.push(arr.slice(index)); return accumulator; }, []);
 
-// Q. [1,2,3,4,5]을 인덱스를 키로 갖고 현재 값의 제곱을 값으로 갖는 배열로 변형
+// Q. 1~10을 인자로 가진 배열의 모든 숫자를 곱하기.
+// range(10, 1).reduce( ... );
+
+
+// Q. [1,2,3,4,5]의 인덱스를 키로 갖고 현재 값의 제곱을 값으로 갖는 배열로 변형
 // A.
 [1,2,3,4,5].reduce((accumulator, current, index) =>{
     var temp = {};
@@ -108,16 +117,18 @@ range(10).reduce((accumulator, currentValue, index, array) => accumulator + curr
 }, []);
 
 
+// Q. 추상화하여 로직을 주입하는 방식으로 바꾸기
+// function reduceFunc(arr, func) { ... }
+// 이런 식으로 실행 reduceFunc([1,2,3], (accum, curr)=>accum+curr);
 function reduceFunc(arr, func) {
     return arr.reduce(func);
 }
-
 reduceFunc([1,2,3], (accum, curr)=>accum+curr);
 
-var reducePlus = (accum, curr)=>accum+curr;
 
-reduceFunc([1,2,3], reducePlus);
-
+// Q. 로직은 고정시키고 배열만 동적으로 받을 수 있도록 바꿔보자
+// function setReduce( ... ) { ... };
+// var reducePlusExec = setReduce( ... );
 function setReduce(func) {
   return function (arr) {
       return arr.reduce(func);
@@ -130,7 +141,7 @@ reducePlusExec([1,2,3,4,5]);
 
 
 /*-------------------------------------------------------------- reduceRight */
-// reduce와 기능 동일. 다만 오른쪽에서 왼쪽으로 배열 순
+// reduce와 기능 동일. 다만 배열의 인자를 읽는 순서가 오른쪽에서 왼쪽으로
 [1,2,3,4,5].reduceRight((accumulator, current, index) =>{
     var temp = {};
     temp[index] = current ** 2;
@@ -155,6 +166,8 @@ range(10).some(ele=>ele>9);
 [12,23,5,2,6,3,4,2].sort();
 
 // comparator는 기본적으로 아래의 로직을 따른다
+
+/*
 function compare(a, b) {
     if (a is less than b by some ordering criterion) {
         return -1;
@@ -165,6 +178,7 @@ function compare(a, b) {
     // a must be equal to b
     return 0;
 }
+*/
 
 
 // [12,23,5,2,,6,3,4,2]을 숫자의 대소비교로 정렬
@@ -176,15 +190,79 @@ function compare(a, b) {
 // [].slice(시작인덱스, 종료인덱스)
 // 원본을 건드리지 않는다
 var arr = [1, 2, 3, 4, 5];
-arr.slice(1, 3);
+var newArr = arr.slice(1, 3);
+console.log(newArr);
 console.log(arr);
 
 
 /*-------------------------------------------------------------- splice */
 // 배열을 시작인덱스부터 갯수만큼 삭제(삭제된 값 리턴)
 // [].slice(시작인덱스, 삭제할 갯수, 추가값1, 추가값2, ...)
-arr.splice(1, 3, 7, 8);
+var newArr = arr.splice(1, 3);
+console.log(newArr);
 console.log(arr);
+
+// 값을 추가하는 경우
+arr = [1, 2, 3, 4, 5];
+newArr = arr.splice(1, 3, 7, 8);
+console.log(newArr);
+console.log(arr);
+
+
+/* 종합문제 */
+var data = range(100, 1);
+
+// Q. data를 앞에서부터 50개의 요소 newData에 복사
+var newData = data.slice(0,50);
+console.log(newData);
+
+// Q. 복사한 데이터 중 뒤의 20개는 삭제하고 삭제한 곳에 99, 100을 넣기
+newData.splice(0, 20, 99, 100);
+console.log(newData);
+
+// Q. 역순으로 정렬
+newData.sort((a, b) => b-a);
+console.log(newData);
+
+// Q. 해당 데이터를 5의 배수만 있는 배열로 변경
+newData = newData.filter(ele=>ele%5===0);
+console.log(newData);
+
+// Q. 해당 데이터가 모두 5의 배수인지 확인
+console.log(newData.every(ele=>ele%5===0));
+
+// Q. 해당 데이터중 하나라도 3의 배수인지 확인
+console.log(newData.some(ele=>ele%3===0));
+
+// Q. 해당 데이터의 3의 배수인 값 가져오기
+console.log(newData.find(ele=>ele%3===0));
+
+// Q. 해당 데이터의 3의 배수인 값의 인덱스 가져오기
+console.log(newData.findIndex(ele=>ele%3===0));
+
+// Q. 해당 데이터를 아래와 같은 형태로 바꾸기
+// [{idx:0, value:100}, {idx:1, value:50} ... ];
+newData.reduce((accum, curr, idx) => {
+    var temp = {
+        idx: idx,
+        value: curr
+    };
+    accum.push(temp);
+    return accum;
+}, []);
+
+// Q. 해당 데이터를 아래와 같은 형태로 바꾸기
+// 예 : [ {0: 100}, {1: 50}, ...]
+newData.reduce((accum, curr, idx) => {
+    var temp = {};
+    temp[idx] = curr;
+    accum.push(temp);
+    return accum;
+}, []);
+
+
+
+
 
 
 
