9da783143f440305f004f0a839994a463d42bf71
yblee <youngbeomrhee@gmail.com>
2017-06-16 18:13:36 +0900

    예제 추가

--- javacafe_2017_frontend/ch008/1_Callbacks/checkInService_tests.js
+++ javacafe_2017_frontend/ch008/1_Callbacks/checkInService_tests.js
@@ -8,19 +8,17 @@ describe('Conference.checkInService', function() {
       checkInRecorder,
       attendee;
 
-  //
   beforeEach(function() {
     checkInRecorder = Conference.checkInRecorder();
-    spyOn(checkInRecorder, 'recordCheckIn');
 
-    // checkInRecorder를 주입하면서
-    // 이 함수의 recordCheckIn 함수에 스파이를 심는다
+    // 이 함수의 recordCheckIn 함수에 스파이를 심고
+    // checkInRecorder를 주입한다
+    spyOn(checkInRecorder, 'recordCheckIn');
     checkInService = Conference.checkInService(checkInRecorder);
 
     attendee = Conference.attendee('형철', '서');
   });
 
-  //
   describe('checkInService.checkIn(attendee)', function() {
     it('참가자를 체크인 처리한 것으로 표시한다', function() {
       checkInService.checkIn(attendee);
