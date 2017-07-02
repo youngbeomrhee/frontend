c76eb8ded420c1bbb0a5130ee349e01089a4e90d
whybe <youngbeomrhee@gmail.com>
2017-07-02 22:27:06 +0900

    리액트 예제 추가

---
+++ react_tic-tac-toe/index.js
@@ -0,0 +1,96 @@
+/**
+ * Created by whybe on 2017. 6. 25..
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+class Square extends React.Component {
+    /*
+    constructor() {
+        super();
+        this.state = {
+            value: null,
+        };
+    }
+    */
+    render() {
+        return (
+            <button className="square" onClick={() => this.props.onClick()}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            squares: Array(9).fill(1),
+        };
+    }
+
+    renderSquare(i) {
+        return <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />;
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] ='X';
+        this.setState({squares: squares});
+    }
+
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
