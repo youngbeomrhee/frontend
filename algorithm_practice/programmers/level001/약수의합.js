c8e3df97ed2566eb6a082a73674c21d6f9c30650
youngbeomrhee <youngbeomrhee@gmail.com>
2018-03-15 13:20:19 +0900

    알고리즘 연습문제 추가

---
+++ algorithm_practice/programmers/level001/약수의합.js
@@ -0,0 +1,36 @@
+/**
+ * Created by yblee on 2018-03-15.
+ */
+/*
+약수의 합
+ 어떤 수를 입력받아 그 수의 약수를 모두 더한 수 sumDivisor 함수를 완성해 보세요.
+ 예를 들어 12가 입력된다면 12의 약수는 [1, 2, 3, 4, 6, 12]가 되고, 총 합은 28이 되므로 28을 반환해 주면 됩니다.
+*/
+
+const getDevisors = (function() {
+    let cache = {};	// 계산결과 cache에 저장
+
+    return function(num) {
+        if(cache[num]) { return cache[num]; };
+        let divisors = [num], divisor = Math.floor(num/2);	// 불필요한 계산 절반으로 줄이기
+
+        while(divisor) {
+            if(num%divisor===0) { divisors.push(divisor); };
+            divisor--;
+        }
+        cache[num] = divisors;
+        return divisors;
+    }
+})();
+
+function sumDivisor(num) {
+    return getDevisors(num).reduce((p,n)=>p+n);
+}
+
+// 아래는 테스트로 출력해 보기 위한 코드입니다.
+console.log(sumDivisor(12));
+console.log(sumDivisor(10));
+console.log(sumDivisor(21));
+console.log(sumDivisor(112349872));
+console.log(sumDivisor(112349872));
+
