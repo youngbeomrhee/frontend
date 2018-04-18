9e857d1d18208025a41272ad9607c5dc3d50192b
whybe <youngbeomrhee@gmail.com>
2018-04-18 23:27:48 +0900

    예제 추가

--- algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/LinkedList.js
+++ algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/LinkedList.js
@@ -4,10 +4,10 @@
 
 
 class Node {
-    constructor(e, p, n) {
-        this.prev = p ? p : null;
-        this.next = n ? n : null;
-        this.ele = e ? e : null;
+    constructor(prev, ele, next) {
+        this.prev = prev ? prev : null;
+        this.next = next ? next : null;
+        this.ele = ele ? ele : null;
     }
 }
 
@@ -21,30 +21,30 @@ class LinkedList {
         }
     }
 
-    linkFirst(e) {
-        const f = this.head,
+    linkFirst(ele) {
+        const first = this.head,
             prev=null,
-            next=f;
-        const newNode = new Node(e, prev, next);
+            next=first,
+            newNode = new Node(prev, ele, next);
         this.head = newNode;
-        if(!f) {
+        if(!first) {
             this.tail = newNode;
         } else {
-            f.prev = newNode;
+            first.prev = newNode;
         }
         this.length++;
     }
 
-    linkLast(e) {
-        const l = this.tail,
-            prev=l,
-            next=null;
-        const newNode = new Node(e, prev, next);
+    linkLast(ele) {
+        const last = this.tail,
+            prev=last,
+            next=null,
+            newNode = new Node(prev, ele, next);
         this.tail = newNode;
-        if(!l) {
+        if(!last) {
             this.head = newNode;
         } else {
-            l.next = newNode;
+            last.next = newNode;
         }
         this.length++;
     }
@@ -58,6 +58,62 @@ class LinkedList {
         if(!this.tail) throw '마지막 요소가 없습니다.';
         return this.tail;
     }
+    indexOf(node) {
+        if(!(node instanceof Node)) throw '잘못된 타입이 들어왔습니다.';
+
+        let curr = this.head,
+            i=0,
+            matchIdx=-1;
+
+        if(!curr) return -1;
+        while(curr.next) {
+            if(curr === node) {
+                matchIdx = i;
+                break;
+            }
+            curr = curr.next;
+            i++;
+        }
+        return matchIdx;
+    }
+    contains(node) {
+        if(!(node instanceof Node)) throw '잘못된 타입이 들어왔습니다.';
+        return this.indexOf(node)<=-1;
+    }
+    linkBefore(ele, succ) {
+        if(!ele || !succ) throw '인자 2개는 필수입니다.';
+        if(!(succ instanceof Node)) throw '잘못된 타입입니다.';
+        if(!this.contains(succ)) throw 'List에 해당 값이 없습니다.';
+
+        const pred = succ.prev,
+            newNode = new Node(pred, ele, succ);
+        succ.prev = newNode;
+        if(!pred) {
+            this.head = newNode;
+        } else {
+            pred.next = newNode;
+        }
+        this.length++;
+    }
+/*
+
+    linkAfter(ele, pred) {
+        if(!ele || !pred) throw '인자 2개는 필수입니다.';
+        if(!(pred instanceof Node)) throw '잘못된 타입입니다.';
+        if(!this.contains(pred)) throw 'List에 해당 값이 없습니다.';
+
+        const succ = pred.next,
+            newNode = new Node(pred, ele, succ);
+        pred.next = newNode;
+        if(!succ) {
+            this.tail = newNode;
+        } else {
+            succ.prev = newNode;
+        }
+        this.length++;
+    }
+*/
+
 
 /*
     linkBefore(ele, succ) {
