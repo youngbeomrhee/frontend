2b1f19412a93b5506da2f6151ba56f5a0e6bf1ea
whybe <youngbeomrhee@gmail.com>
2018-04-28 08:55:54 +0900

    예제 추가 및 수정

--- algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/LinkedList.js
+++ algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/LinkedList.js
@@ -1,16 +1,6 @@
 /**
  * Created by yblee on 2018-04-10.
  */
-const MSG = {};
-MSG.errParam = '필수인자가 누락되었습니다.';
-
-function errParam() {
-    throw MSG.errParam;
-}
-
-function typeCheck(instance, type) {
-    if(!(instance instanceof type)) throw new TypeError(type);
-}
 
 function require(...args) {
     let nullEles = args.reduce((accum, curr)=>{
