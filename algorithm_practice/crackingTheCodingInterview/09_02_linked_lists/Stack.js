5279e885e981acf043927b0e2e00597fbf30e953
youngbeomrhee <youngbeomrhee@gmail.com>
2018-05-14 17:48:30 +0900

    예제 추가

---
+++ algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/Stack.js
@@ -0,0 +1,22 @@
+/**
+ * Created by yblee on 2018-05-14.
+ */
+class Stack {
+    constructor {
+    }
+    push(item) {
+
+    }
+    pop() {
+
+    }
+    peek() {
+
+    }
+    empty() {
+
+    }
+    search(obj) {
+
+    }
+}
