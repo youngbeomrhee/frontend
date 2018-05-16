f5abcc0dc0e112da7d9a30c895393ceeaf77e93f
youngbeomrhee <youngbeomrhee@gmail.com>
2018-05-16 16:50:39 +0900

    예제 추가

--- algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/Stack.js
+++ algorithm_practice/crackingTheCodingInterview/09_02_linked_lists/Stack.js
@@ -2,21 +2,56 @@
  * Created by yblee on 2018-05-14.
  */
 class Stack {
-    constructor {
+    constructor(arr) {
+        this.data = [];
+        if(arr) {
+            this.addAll(arr);
+        }
     }
-    push(item) {
-
+    size() {
+        return this.data.length;
+    }
+    push(item=errParam()) {
+        this._addElement(item);
+        return item;
     }
     pop() {
-
+        const obj = this.peek(),
+            len = this.size();
+        if(len < 1) throw new RangeError(len);
+        this._removeElementAt(len - 1);
+        return obj;
     }
     peek() {
-
+        const len = this.size();
+        if(len < 1) throw new RangeError(len);
+        return this._elementAt(len - 1);
     }
-    empty() {
-
+    isEmpty() {
+        return this.size() === 0;
     }
     search(obj) {
+        const i = this._lastIndexOf(obj);
+        if(i >= 0) {
+            return this.size() - i;
+        }
+        return -1;
+    }
+    addAll(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            this.push(arr[i]);
+        }
+    }
+    _addElement(item=errParam()) {
+        this.data.push(item);
+    }
+    _removeElementAt(i) {
+
+    }
+    _elementAt(i) {
+
+    }
+    _lastIndexOf(obj) {
 
     }
 }
