f4df39d4e8ef3cc2e2e6eb065b06fa9db13be5dc
whybe <youngbeomrhee@gmail.com>
2018-10-01 21:14:36 +0900

    예제 추가

---
+++ nodejs_textbook/ch04_http/4.1_request_response/server2.js
@@ -0,0 +1,11 @@
+const http = require('http'),
+    fs = require('fs');
+
+http.createServer((req, res) => {
+    fs.readFile('./server2.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+}).listen(8081, () => {
+    console.log('8081번 포트에서 서버 대기 중입니다.');
+});
---
+++ nodejs_textbook/ch04_http/4.2_cookie_session/server3.js
@@ -0,0 +1,29 @@
+const http = require('http'),
+    parseCookies = (cookie='') => (
+        cookie
+            .split(';')
+            .map(v => v.split('='))
+            .map(([k, ...vs]) => [k, vs.join('=')])
+            .reduce((acc, [k, v]) => {
+                acc[k.trim()] = decodeURIComponent(v);
+                return acc;
+            }, {})
+    );
+
+http.createServer((req, res) => {
+    const cookieStr = req.headers.cookie,
+        cookies = parseCookies(cookieStr);
+    // console.log("cookieStr ->", cookieStr);
+    console.log("req.url ->", req.url);
+    console.log("cookies ->", cookies);
+    res.writeHead(200, {
+        'Set-cookie': ['mycookie4=test4','mycookie5=test5']
+    });
+    res.end('Hello cookie');
+}).listen(8082, () => {
+    console.log('8082번 포트에서 서버가 대기중입니다.');
+});
+
+
+
+
